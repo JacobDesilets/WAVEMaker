2c855fecb3ae5dede8395fd0b99f40eb1000fd3c
JacobDesilets <jacobdesilets@gmail.com>
2022-09-26 23:44:45 -0400

    got numpy version working

--- WAVEMaker.py
+++ WAVEMaker.py
@@ -26,26 +26,31 @@ def get_sin_wave_generator(freq, sample_rate, amplitude):
 def get_sin_channel(freq, sample_rate, amplitude, n_samples, bits_per_sample):
     sin_generator = get_sin_wave_generator(freq, sample_rate, amplitude)
     max_amplitude = ((1 << bits_per_sample) / 2) - 1
-    # channel = []
     channel = bytearray()
+
+    bits_per_sample_rounded = (bits_per_sample + 7) & (-8)  # Round up to the nearest multiple of 8
+    unused_bits = bits_per_sample_rounded - bits_per_sample
 
     for i in range(n_samples):
         channel.extend(int(max_amplitude * next(sin_generator)).to_bytes(bits_per_sample, 'little', signed=True))
+        for j in range(unused_bits):
+            channel.append(1)
 
     return channel
 
 
 def get_channels(n_channels, sample_rate, bits_per_sample, n_samples, mode='sine'):
-
+    start = perf_counter()
     if mode != 'sine':
         sys.exit('Not yet implemented')
     else:
         data = bytearray()
         channels = []
+        bps_r = (bits_per_sample + 7) & (-8)  # Round up to the nearest multiple of 8
         for i in range(n_channels):
-            c = bytes(get_sin_channel(10000, sample_rate, 1, n_samples, bits_per_sample))
-            channels.append([c[k:k+bits_per_sample] for k in range(0, len(c), bits_per_sample)])
-        start = perf_counter()
+            c = bytes(get_sin_channel(2048, sample_rate, 1, n_samples, bps_r))
+            channels.append([c[k:k+(bps_r//8)] for k in range(0, len(c), (bps_r//8))])
+
         for i in range(n_samples):
             for channel in channels:
                 data.extend(channel[i])
@@ -57,19 +62,22 @@ def get_channels(n_channels, sample_rate, bits_per_sample, n_samples, mode='sine
 def get_header_chunk(fmt, sample_rate, bits_per_sample, n_channels, n_samples):
     """Generates Header and Format chunks"""
     chunk_size = None
+    bps_r = (bits_per_sample + 7) & (-8)  # Round up to the nearest multiple of 8
+
     if fmt == 1:  # PCM
-        chunk_size = 36 + (n_samples * n_channels * bits_per_sample // 8)
+        chunk_size = 36 + (n_samples * n_channels * bps_r // 8)
         header = bytes('RIFF', 'ascii')                                                                         # 0     RIFF Chunk ID
         header += chunk_size.to_bytes(length=4, byteorder='little')                                             # 4     Filesize - 8
         header += bytes('WAVE', 'ascii')                                                                        # 8     Format code (WAVE)
+
         header += bytes('fmt ', 'ascii')                                                                        # 12    Format Chunk ID
         header += int(16).to_bytes(length=4, byteorder='little')                                                # 16    Format Chunk Size (16 for PCM)
         header += fmt.to_bytes(length=2, byteorder='little')                                                    # 20    Format Code (1 for PCM)
         header += n_channels.to_bytes(length=2, byteorder='little')                                             # 22    Number of channels
         header += sample_rate.to_bytes(length=4, byteorder='little')                                            # 24    Sample rate
-        header += (sample_rate * n_channels * bits_per_sample // 8).to_bytes(length=4, byteorder='little')      # 28    Byte rate
-        header += (n_channels * bits_per_sample // 8).to_bytes(length=2, byteorder='little')                    # 32    Block allign: The number of bytes for one sample across all channels
-        header += bits_per_sample.to_bytes(length=2, byteorder='little')                                        # 34    Bits per sample
+        header += (sample_rate * n_channels * bps_r // 8).to_bytes(length=4, byteorder='little')                # 28    Byte rate
+        header += (n_channels * bps_r // 8).to_bytes(length=2, byteorder='little')                              # 32    Block allign: The number of bytes for one sample across all channels
+        header += bps_r.to_bytes(length=2, byteorder='little')                                                  # 34    Bits per sample
         return header
 
     else:
@@ -77,9 +85,9 @@ def get_header_chunk(fmt, sample_rate, bits_per_sample, n_channels, n_samples):
 
 
 def get_pcm_data_chunk(sample_rate, bits_per_sample, n_channels, n_samples):
-
+    bps_r = (bits_per_sample + 7) & (-8)  # Round up to the nearest multiple of 8
     data = bytes('data', 'ascii')
-    data += (n_samples * n_channels * bits_per_sample // 8).to_bytes(length=4, byteorder='little')
+    data += (n_samples * n_channels * bps_r // 8).to_bytes(length=4, byteorder='little')
     samples = get_channels(n_channels, sample_rate, bits_per_sample, n_samples)
     start = perf_counter()
     # for sample in samples:
@@ -102,7 +110,7 @@ def make_file(fname, sample_rate, bits_per_sample, n_channels, duration):
 def main():
     # args = vars(get_args())
     start = perf_counter()
-    make_file('test.wav', 44100, 16, 3, 5)
+    make_file('test.wav', 44100, 24, 2, 3)
     stop = perf_counter()
     print(f'Time taken: {stop - start}')
 
---
+++ WAVEMaker_np.py
@@ -0,0 +1,83 @@
+import argparse
+import math
+from pathlib import Path
+import sys
+from time import perf_counter
+import numpy as np
+
+
+class Wave:
+    def __init__(self, sample_rate=44100, bit_depth=16, n_channels=2, duration=3, fmt='PCM'):
+        self.sample_rate = sample_rate                      # Sample rate in Hz
+        self.bit_depth = bit_depth                          # Bits per sample
+        self.n_channels = n_channels                        # Channel count
+        self.duration = duration                            # Length of audio in seconds
+        self.n_samples = self.duration * self.sample_rate
+        self.fmt = fmt                                      # Compression format (String)
+
+        self.bit_depth_rounded = (self.bit_depth + 7) & (-8)  # Bits per sample must be multiple of 8
+        self.np_dt = f'<i{int(self.bit_depth_rounded / 8)}'
+        self.data = np.zeros((self.n_samples, self.n_channels), dtype=self.np_dt)
+
+    def get_header_chunk(self):
+        chunk = bytes('RIFF', 'ascii')
+        chunk_size = 36 + (self.n_samples * self.n_channels * self.bit_depth_rounded // 8)
+        chunk += chunk_size.to_bytes(length=4, byteorder='little')
+        chunk += bytes('WAVE', 'ascii')
+
+        return chunk
+
+    def get_fmt_chunk(self):
+        chunk = bytes('fmt ', 'ascii')
+
+        if self.fmt == 'PCM':
+            chunk += int(16).to_bytes(length=4, byteorder='little')
+            chunk += int(1).to_bytes(length=2, byteorder='little')
+            chunk += self.n_channels.to_bytes(length=2, byteorder='little')
+            chunk += self.sample_rate.to_bytes(length=4, byteorder='little')
+            byte_rate = (self.sample_rate * self.n_channels * self.bit_depth_rounded // 8)
+            chunk += byte_rate.to_bytes(length=4, byteorder='little')
+            block_align = (self.n_channels * self.bit_depth_rounded // 8)
+            chunk += block_align.to_bytes(length=2, byteorder='little')
+            chunk += self.bit_depth_rounded.to_bytes(length=2, byteorder='little')
+        else:
+            sys.exit('Non-PCM compression not yet implemented')
+            # TODO: Implement
+
+        return chunk
+
+    def get_pcm_data_chunk(self):
+        chunk = bytes('data', 'ascii')
+        chunk_size = int(self.n_samples * self.n_channels * self.bit_depth_rounded / 8)
+        chunk += chunk_size.to_bytes(length=4, byteorder='little')
+        interleaved_bytes = np.column_stack(self.data.tolist()).astype(self.np_dt).tobytes()
+        chunk += interleaved_bytes
+
+        return chunk
+
+    def make_file(self, fname):
+        fp = Path(fname)
+        with open(fp, 'wb') as f:
+            f.write(self.get_header_chunk())
+            f.write(self.get_fmt_chunk())
+            if self.fmt == 'PCM':
+                f.write(self.get_pcm_data_chunk())
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='WAVE Maker')
+    parser.add_argument('file', metavar='File', type=str, nargs=1, help='WAVE file to work on')
+    parser.add_argument('-c', metavar='Comp', type=int, nargs='?', const=1, help='Compression format (1 is PCM and default)')
+
+    return parser.parse_args()
+
+
+def main():
+    start = perf_counter()
+    wavemaker = Wave(bit_depth=64, n_channels=2, duration=3)
+    wavemaker.make_file('test2.wav')
+    print(f'Finished in {perf_counter() - start} seconds.')
+
+
+if __name__ == '__main__':
+    main()
